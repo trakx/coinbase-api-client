d3775f93438087fd4c743322cf62e2d59a3e18d5
AymericNoel <aym.noel@gmail.com>
2021-01-19 18:41:28 +0100

    Add ReadMe attribute

--- src/Trakx.Coinbase.Custody.ApiClient/CoinbaseCustodyApiConfiguration.cs
+++ src/Trakx.Coinbase.Custody.ApiClient/CoinbaseCustodyApiConfiguration.cs
@@ -1,14 +1,27 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using Trakx.Utils.Attributes;
 
 namespace Trakx.Coinbase.Custody.ApiClient
 {
     public class CoinbaseCustodyApiConfiguration
     {
 #nullable disable
-        [Required] public string BaseUrl { get; set; }
-        [Required] public string AccessKey { get; set; }
-        [Required] public string PassPhrase { get; set; }
+        [Required]
+        [SecretEnvironmentVariable("CoinbaseCustodyApiConfiguration__BaseUrl")]
+        public string BaseUrl { get; set; }
+
+        [Required]
+        [SecretEnvironmentVariable("CoinbaseCustodyApiConfiguration__AccessKey")]
+        public string AccessKey { get; set; }
+
+        [Required]
+        [SecretEnvironmentVariable("CoinbaseCustodyApiConfiguration__PassPhrase")] 
+        public string PassPhrase { get; set; }
+
+        [SecretEnvironmentVariable("CoinbaseCustodyApiConfiguration__InitialRetryDelayInMilliseconds")]
         public int? InitialRetryDelayInMilliseconds { get; set; }
+
+        [SecretEnvironmentVariable("CoinbaseCustodyApiConfiguration__MaxRetryCount")]
         public int? MaxRetryCount { get; set; }
 #nullable restore
     }
